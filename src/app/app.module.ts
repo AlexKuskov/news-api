4d50ad0f8aa63ceb15fee40087386cf5b538ea27
FalseNumber <sasha.sanya.aleksandr@gmail.com>
2019-08-10 23:18:25 +0300

    Add DateRangePicker library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { DateRangePickerModule } from '@syncfusion/ej2-angular-calendars';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -16,7 +17,8 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    DateRangePickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
